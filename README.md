2a89ed68ba19f2a523fb8fb2a22c3cfce6205739
r4lly99 <mohd.rully.k@gmail.com>
2020-10-07 12:51:36 +0700

    firs commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# spring-mysql-redis
-Spring Boot with MySQL and Redis Cache
+# Spring-Mysql-Redis
+Spring Boot Mysql Redis REST API Cache example
+
+### Prerequisites
+- JDK 1.8
+- Maven
+- Mysql
+- Redis (Using Lettuce)
+
+## Quick Start
+
+```
+start MySQL service and create database
+```
+
+```
+start Redis service
+```
+
+### Build
+```
+mvn clean package
+```
+
+### Run
+```
+java -jar target/spring-mysql-redis.jar
+```
+
+##
+### Get information about system health, configurations, etc.
+```
+http://localhost:8091/env
+http://localhost:8091/health
+http://localhost:8091/info
+http://localhost:8091/metrics
+```
+
+
+### Test using CURL
+
+- data add
+```
+curl --location --request POST 'http://localhost:8080/api/employees' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name" : "Yusran Fikri",
+    "age"  : 10 ,
+    "salary" : 2700 
+}'
+```
+
+- data get cache expire 3000 ms
+```
+curl --location --request GET 'http://localhost:8080/api/employees/4'
+```
+
+- response : 
+```
+{
+    "id": 4,
+    "createdDate": "2020-10-07T11:37:39",
+    "modifiedDate": "2020-10-07T11:37:39",
+    "name": "Yusran Fikri",
+    "age": 10,
+    "salary": 2700.0
+}
+```
+
+
+### Swagger-ui REST API Reference & Test
+- http://localhost:8080/swagger-ui.html
+- Response Content Type : application/json
--- src/main/java/com/irsyad/springmysqlredis/config/RedisConfig.java
+++ src/main/java/com/irsyad/springmysqlredis/config/RedisConfig.java
@@ -1,20 +1,20 @@
 package com.irsyad.springmysqlredis.config;
 
+import io.lettuce.core.ClientOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.redis.pool.max-active}")
-    private int maxPool;
 
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -22,17 +22,21 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        final JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(maxPool);
-        poolConfig.setTestWhileIdle(true);
+    private @Value("${spring.redis.timeout}")
+    Duration redisCommandTimeout;
 
-        final JedisConnectionFactory connectionFactory = new JedisConnectionFactory(poolConfig);
-        connectionFactory.setUsePool(true);
-        connectionFactory.setHostName(redisHost);
-        connectionFactory.setPort(redisPort);
-        return connectionFactory;
+    @Value("${spring.redis.pool.max-active}")
+    private int maxPool;
+
+    @Bean
+    protected LettuceConnectionFactory redisConnectionFactory() {
+        final ClientOptions clientOptions = ClientOptions.builder().build();
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(redisCommandTimeout)
+                .clientOptions(clientOptions).build();
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(redisHost,
+                redisPort);
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
     @Bean
